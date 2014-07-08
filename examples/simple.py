c7727e83399a4fd35dcd272b8471c0b47511ff9f
xue peng <xuepeng@tianyin-h8-1160t.(none)>
2014-07-08 16:12:16 -0700

    update example

--- examples/simple.py
+++ examples/simple.py
@@ -5,8 +5,12 @@ import sys
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 poj_dir = os.path.dirname(cur_dir)
 sys.path.append(os.path.join(poj_dir, 'python/'))
-print sys.path
 import cmd_wrapper
+
+httpd_dir= os.path.join(poj_dir, 'data/httpd/')
+httpd_index_dir = os.path.join(httpd_dir, 'index/')
+if not os.path.exists(httpd_index_dir):
+    os.mkdir(httpd_index_dir)
 
 
 
@@ -15,5 +19,11 @@ cmd_wrapper.compile()
 
 if __name__ == '__main__':
     query = "return default 443 vhost"
-    # do squery here
-
+    popularity_file = None
+    cmd_wrapper.execute_index(httpd_index_dir, os.path.join(httpd_dir, 'parameters/'), popularity_file)
+    tmp_in_fp = os.path.join(cur_dir, 'tmp.input')
+    open(tmp_in_fp, 'w').write(query)
+    tmp_out_fp = os.path.join(cur_dir, 'tmp.output')
+    cmd_wrapper.execute_search(httpd_index_dir, tmp_in_fp, tmp_out_fp, True)
+    res = cmd_wrapper.extract_search_outputfile(tmp_out_fp)
+    print res
