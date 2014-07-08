ff04efa7f2ab39ac085c8a61ade791df39225c58
Xuepeng Fan <athrunarthur@gmail.com>
2014-07-08 13:02:01 -0700

    add simple example

---
+++ examples/simple.py
@@ -0,0 +1,19 @@
+import os
+import json
+import csv
+import sys
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+poj_dir = os.path.dirname(cur_dir)
+sys.path.append(os.path.join(poj_dir, 'python/'))
+print sys.path
+import cmd_wrapper
+
+
+
+cmd_wrapper.compile()
+
+
+if __name__ == '__main__':
+    query = "return default 443 vhost"
+    # do squery here
+
