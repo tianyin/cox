b115048e52756aa3bb1a697d04800fb349603e94
Tianyin Xu <tixu@cs.ucsd.edu>
2014-12-27 15:55:07 -0800

    use relative paths

--- benchmarks/main.py
+++ benchmarks/main.py
@@ -7,30 +7,30 @@ from evaluator import eval_wrap
 
 if __name__ == '__main__':
     appname  = 'httpd'
-    parpath  = '/home/tianyin/cox/dataset/httpd/parameters'
-    poppath  = '/home/tianyin/cox/benchmarks/data/apache_popularity.csv' 
-    p2mmaps  = '/home/tianyin/cox/benchmarks/data/apache_modules.csv'
-    testcase = '/home/tianyin/cox/benchmarks/data/apache_tc_icon.csv' 
-    base_dir = '/home/tianyin/cox/benchmarks/data/' + appname
+    parpath  = '../dataset/httpd/parameters'
+    poppath  = '../benchmarks/data/apache_popularity.csv' 
+    p2mmaps  = '../benchmarks/data/apache_modules.csv'
+    testcase = '../benchmarks/data/apache_tc_icon.csv' 
+    base_dir = '../benchmarks/data/' + appname
     config = EvalConfig(appname, parpath, poppath, p2mmaps, None, testcase, base_dir)
     eval_wrap(config)
 
     appname  = 'hadoop'
-    parpath  = '/home/tianyin/cox/dataset/hadoop/parameters'
-    poppath  = '/home/tianyin/cox/benchmarks/data/hadoop_popularity.csv' 
+    parpath  = '../dataset/hadoop/parameters'
+    poppath  = '../benchmarks/data/hadoop_popularity.csv' 
     p2mmaps  = None
-    rnmmaps  = '/home/tianyin/cox/benchmarks/data/hadoop_rename.txt'
-    testcase = '/home/tianyin/cox/benchmarks/data/hadoop_tc_icon.csv' 
-    base_dir = '/home/tianyin/cox/benchmarks/data/' + appname
+    rnmmaps  = '../benchmarks/data/hadoop_rename.txt'
+    testcase = '../benchmarks/data/hadoop_tc_icon.csv' 
+    base_dir = '../benchmarks/data/' + appname
     config = EvalConfig(appname, parpath, poppath, p2mmaps, rnmmaps, testcase, base_dir)
     #eval_wrap(config)
 
     appname  = 'mysql'
-    parpath  = '/home/tianyin/cox/dataset/mysql/parameters'
-    poppath  = '/home/tianyin/cox/benchmarks/data/mysql_popularity.csv' 
+    parpath  = '../dataset/mysql/parameters'
+    poppath  = '../benchmarks/data/mysql_popularity.csv' 
     p2mmaps  = None
-    testcase = '/home/tianyin/cox/benchmarks/data/mysql_tc_icon.csv' 
-    base_dir = '/home/tianyin/cox/benchmarks/data/' + appname
+    testcase = '../benchmarks/data/mysql_tc_icon.csv' 
+    base_dir = '../benchmarks/data/' + appname
     config = EvalConfig(appname, parpath, poppath, p2mmaps, None, testcase, base_dir)
     #eval_wrap(config)
 
